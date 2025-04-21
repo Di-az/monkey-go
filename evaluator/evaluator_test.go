f6a7104060b59fcc7e47e64c82abbea7b97d56dc
cadidiaz <carlos.c.diaz@oracle.com>
2025-04-21 17:21:08 -0600

    Support for string comparison

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -210,6 +210,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -313,5 +317,37 @@ func TestStringLiteral(t *testing.T) {
 
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+
+	str, ok := evaluated.(*object.String)
+
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringComparation(t *testing.T) {
+	input := `"Hello" != "World"`
+
+	evaluated := testEval(input)
+
+	result, ok := evaluated.(*object.Boolean)
+
+	if !ok {
+		t.Fatalf("object is not Boolean. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if result.Value != true {
+		t.Errorf("object has wrong value. got=%t, want=%t", result.Value, true)
 	}
 }
